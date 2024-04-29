fb762bb24c5311852726e89ed704309abea0ac1e
sneha9044 <sr23.rkt@gmail.com>
2024-04-29 22:03:02 +0530

    work

---
+++ practice/circumfrenceofcircle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int circumfrence, r;
+    cout<<"value of r: ";
+    cin>>r;
+    circumfrence=2*3.14*r;
+    cout<<"circumfrence of circle: "<<circumfrence;
+    return 0;
+}
