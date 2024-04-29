38f0ba4de45143345900b8067396dea46eea9523
sneha9044 <sr23.rkt@gmail.com>
2024-04-29 20:53:59 +0530

    work

---
+++ practice/si.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int si,p,r,t;
+    cout<<"enter the value of p";
+    cin>>p;
+    cout<<"enter the value of r";
+    cin>>r;
+    cout<<"enter the value of t";
+    cin>>t;
+    si=p*r*t/100;
+    cout<<"the value of si"<<si;
+    return 0;
+}
