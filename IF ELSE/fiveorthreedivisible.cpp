70cfd74fab8f7a9e5aa7f93c838fd9ebffff150c
sneha9044 <sr23.rkt@gmail.com>
2024-05-08 15:55:59 +0530

    work

---
+++ IF ELSE/fiveorthreedivisible.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a;
+ cout<<"enter";
+ cin>>a;
+ if(a%5==0&&a%3==0){
+    cout<<"dv";
+ }
+    else{
+        cout<<"ndv";
+    }
+    return 0;
+ }
---
+++ IF ELSE/threedigitno.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,a;
+    cout<<"enter the no";
+    cin>>n;
+    if((a<1000)&&(a<99))
+    {
+        cout<<" its three digit no";
+    }
+    else{
+        cout<<"its not three digit no";
+    }
+    return 0;
+}
